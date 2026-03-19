98697b9acbeab8dd7fad2f84e828287bd4a97bbb
Maxsuel Einstein <maxsueleinstein.dev@gmail.com>
2026-03-19 16:45:02 -0300

    Adiciona validação da lista de Atividades

--- src/main/java/br/com/example/RoteiroViagens/dto/ViagemRequestDTO.java
+++ src/main/java/br/com/example/RoteiroViagens/dto/ViagemRequestDTO.java
@@ -1,6 +1,7 @@
 package br.com.example.RoteiroViagens.dto;
 
 import br.com.example.RoteiroViagens.domain.Atividade;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.PositiveOrZero;
 
@@ -8,5 +9,5 @@ import java.util.List;
 
 public record ViagemRequestDTO(@NotBlank String destino,
                                @PositiveOrZero double orcamentoTotal,
-                               List<Atividade> atividades) {
+                               @Valid List<Atividade> atividades) {
 }
