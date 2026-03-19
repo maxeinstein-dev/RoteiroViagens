bfcbdf7d3522a0309a673bf2e1f21dc25542deff
Maxsuel Einstein <maxsueleinstein.dev@gmail.com>
2026-03-19 16:32:29 -0300

    Adiciona ViagemRequestDTO com as validações iniciais

---
+++ src/main/java/br/com/example/RoteiroViagens/dto/ViagemRequestDTO.java
@@ -0,0 +1,12 @@
+package br.com.example.RoteiroViagens.dto;
+
+import br.com.example.RoteiroViagens.domain.Atividade;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
+
+import java.util.List;
+
+public record ViagemRequestDTO(@NotBlank String destino,
+                               @PositiveOrZero double orcamentoTotal,
+                               List<Atividade> atividades) {
+}
