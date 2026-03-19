9772c2175e7530a7fa40af3af5a78383418ac8ed
Maxsuel Einstein <maxsueleinstein.dev@gmail.com>
2026-03-19 16:33:35 -0300

    Adiciona método obterRoteiroPorId e ajusta método criarNovaViagem para usar ViagemRequestDTO com as validações

--- src/main/java/br/com/example/RoteiroViagens/controller/ViagemController.java
+++ src/main/java/br/com/example/RoteiroViagens/controller/ViagemController.java
@@ -1,11 +1,17 @@
 package br.com.example.RoteiroViagens.controller;
 
 import br.com.example.RoteiroViagens.domain.Viagem;
+import br.com.example.RoteiroViagens.dto.ViagemRequestDTO;
 import br.com.example.RoteiroViagens.service.ViagemService;
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.validation.Valid;
+import org.apache.catalina.valves.rewrite.ResolverImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/viagens")
@@ -18,14 +24,21 @@ public class ViagemController {
         this.viagemService = viagemService;
     }
 
-    @GetMapping("/roteiro")
+    @GetMapping("/roteiros")
     public List<Viagem> obterRoteiro(){
         return viagemService.listarTodas();
     }
 
     @PostMapping
-    public Viagem criarNovaViagem(@RequestBody Viagem novaViagem){
+    public Viagem criarNovaViagem(@Valid @RequestBody ViagemRequestDTO novaViagem){
         return viagemService.criarViagem(novaViagem);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Viagem> obterRoteiroPorId(@PathVariable Long id){
+        Optional<Viagem> viagemPesquisada = viagemService.listarPorId(id);
+        return viagemPesquisada.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+
     }
 
 }
